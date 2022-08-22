eb9e92f1953a12b506546224f01e8dc0b3c437bb
terapark <79031840+terapark@users.noreply.github.com>
2022-08-23 03:53:10 +0900

    Add NET Framework4.8

---
+++ WindowsFormsApp_NET Framwork48/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp_NET_Framwork48
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_login = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(98, 33);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 0;
+            this.button_login.Text = "로그인";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 175);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp_NET Framwork48/Form1.cs
@@ -0,0 +1,47 @@
+﻿using KHOpenApi.NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_NET_Framwork48
+{
+    public partial class Form1 : Form
+    {
+        // ocx인터페이스 추가
+        AxKHOpenAPI axKHOpenAPI;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 새로 추가
+            axKHOpenAPI = new AxKHOpenAPI();
+            axKHOpenAPI.OnEventConnect += new _DKHOpenAPIEvents_OnEventConnectEventHandler(axKHOpenAPI_OnEventConnect);
+            Controls.Add(axKHOpenAPI);
+        }
+
+        // 로그인 이벤트 핸들러
+        private void axKHOpenAPI_OnEventConnect(object sender, _DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                textBox1.Text = "로그인 성공";
+            }
+            else
+            {
+                textBox1.Text = "로그인 실패";
+            }
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            // 로그인 요청
+            axKHOpenAPI.CommConnect();
+        }
+    }
+}
