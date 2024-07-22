9b4f1132a92b90b347a1f716ee0e6fec7b636b7b
teranum <tera63614@gmail.com>
2024-07-22 09:25:22 +0900

    update samples

---
+++ Samples/Console_Async/21. 주식실시간체결.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp;
+
+internal class _21 : SampleBase
+{
+    public override async Task ActionImplement()
+    {
+        // 이벤트 OnReceiveRealData 구독은 SampleBase에서 처리되었음
+
+        // 삼성전자 실시간 시세 요청
+        api.SetRealReg("1000", "005930", "10", "0");
+
+        await GetInputAsync("실시간 시세 작동중, 중지 하려면 아무키나 누르세요");
+
+        api.DisconnectRealData("1000");
+    }
+}
+
+// Output:
+/*
+*/
