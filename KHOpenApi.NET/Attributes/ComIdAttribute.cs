c3ede912a1154e93adf5dcce47d794c482318527
teranum <tera63614@gmail.com>
2025-11-02 00:48:21 +0900

    ComIdAttribute 클래스 추가
    KHOpenApi.NET.Attributes 네임스페이스를 추가하고, 클래스에 연결할 COM 식별자를 지정하기 위한 사용자 정의 특성인 ComIdAttribute 클래스를 구현하였습니다. 이 클래스는 Attribute를 상속받으며, 클래스에만 적용될 수 있도록 AttributeTargets.Class로 제한되었습니다. 생성자 매개변수로 전달된 comId 값을 읽기 전용 속성인 Value에 저장합니다.

---
+++ KHOpenApi.NET/Attributes/ComIdAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace KHOpenApi.NET.Attributes;
+
+/// <summary>
+/// 대상 클래스에 연결할 COM 식별자(ProgID 또는 CLSID)를 지정하는 특성입니다.
+/// </summary>
+/// <param name="comId">대상 클래스에 매핑할 COM 식별자 문자열입니다.</param>
+[AttributeUsage(AttributeTargets.Class)]
+public class ComIdAttribute(string comId) : Attribute
+{
+    /// <summary>
+    /// 지정된 COM 식별자 값입니다.
+    /// </summary>
+    public string Value { get; } = comId;
+}
